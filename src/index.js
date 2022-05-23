1edae60d3f5570d28fcfae8e98b252454d73b4ae
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-05-23 13:46:51 -0500

    Reto 1 con pwa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {IntlProvider} from 'react-intl';
 import JobsList from "./components/jobslist";
 import localeEsMessages from "./locales/es";
 import localeEnMessages from "./locales/en";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 var lng = (navigator.language)
 lng = lng.charAt(0) + lng.charAt(1)
 lng = lng.toLowerCase()
-ReactDOM.render(
-	<IntlProvider locale={lng} messages= {lng==="es"? localeEsMessages: localeEnMessages}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<IntlProvider locale={lng} messages= {lng==="es"? localeEsMessages: localeEnMessages}>
 		<JobsList/>
-	</IntlProvider>, document.getElementById("root")
-	
+</IntlProvider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
